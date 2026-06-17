425e212d32176e291707668b929e621ffed59653
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-17 17:22:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.BackgroundColor=ConsoleColor.White;
+Console.ForegroundColor=ConsoleColor.Black;
+Console.WriteLine("Hello, World!");
